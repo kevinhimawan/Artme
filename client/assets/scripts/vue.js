5055ad792970d8387b726b8a15781fa305b25cc4
fzgitara <fachrizalgita@gmail.com>
2018-03-15 22:24:44 +0700

    tambahan

--- client/assets/scripts/vue.js
+++ client/assets/scripts/vue.js
@@ -3,14 +3,18 @@ new Vue ({
     data: {
         postImages: [
             {
+                id: '01',
                 title: 'Broccoli House',
                 image: 'https://wallpaperbrowse.com/media/images/750806.jpg',
-                desc: 'This is a broccoli house'
+                desc: 'This is a broccoli house',
+                author: 'cecha'
             },
             {
+                id: '02',
                 title: 'Plane On a Forest',
                 image: 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=85740266a52ea733187e9775fdf8d2d5&w=1000&q=80',
-                desc: 'Pesawatnya goblok'
+                desc: 'Pesawatnya goblok',
+                author: 'kevin'
             }
         ]
     }

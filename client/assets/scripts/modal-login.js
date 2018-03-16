21485ff2a5459cec153e40a633df1b7424367e0f
fzgitara <fachrizalgita@gmail.com>
2018-03-16 11:31:21 +0700

    component card and modal login

---
+++ client/assets/scripts/modal-login.js
@@ -0,0 +1,19 @@
+Vue.component ('modal-login', {
+    template: `
+    <div class="modal fade" id="LoginModalCenter" tabindex="-1" role="dialog" aria-labelledby="LoginModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content" style="background-color: #d3d3d3;">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="LoginModalCenterTitle">Login To Your Account</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+                <div class="modal-body" style="background-color: #fff">
+                    <p class="text-center">Login Here</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+})
